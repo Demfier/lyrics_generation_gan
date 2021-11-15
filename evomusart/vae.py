2765b76ed481f4eafd0b01ffb04423016ab27544
Gaurav <sahu.gaurav719@gmail.com>
2021-11-14 23:58:48 -0500

    Add specVAE for evomusart (same as in image_vae, will remove later on)

---
+++ evomusart/vae.py
@@ -0,0 +1,75 @@
+import torch
+import torch.nn as nn
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class Flatten(nn.Module):
+    def forward(self, input):
+        return input.view(input.size(0), -1)
+
+class UnFlatten(nn.Module):
+    def forward(self, input, size=256):
+        return input.view(input.size(0), size, 10, 10)
+
+class VAE(nn.Module):
+    """docstring for VariationalAutoencoder"""
+    def __init__(self, image_channels=3, h_dim=25600, z_dim=128):
+        super(VAE, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(image_channels, 32, kernel_size=6, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=6, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, kernel_size=6, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(128, 256, kernel_size=6, stride=2),
+            nn.ReLU(),
+            Flatten()
+        )
+
+        self.fc1 = nn.Linear(h_dim, z_dim)
+        self.fc2 = nn.Linear(h_dim, z_dim)
+        self.fc3 = nn.Linear(z_dim, h_dim)
+        #self.fc4 = nn.Linear(h_dim, 1024)
+        
+        self.decoder = nn.Sequential(
+            UnFlatten(),
+            nn.ConvTranspose2d(256, 128, kernel_size=6, stride=2),
+            nn.ReLU(),
+            nn.ConvTranspose2d(128, 64, kernel_size=6, stride=2),
+            nn.ReLU(),
+            nn.ConvTranspose2d(64, 32, kernel_size=7, stride=2),
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, image_channels, kernel_size=8, stride=2),
+            nn.Sigmoid(),
+        )
+
+    def reparameterize(self, mu, logvar):
+        std = logvar.mul(0.5).exp_()
+        # return torch.normal(mu, std)
+        epsilon = torch.randn(*mu.size()).to(device)
+        z = mu + std * epsilon
+        return z
+
+    def bottleneck(self, h):
+        mu, logvar = self.fc1(h), self.fc2(h)
+        z = self.reparameterize(mu, logvar)
+        return z, mu, logvar
+
+    def encode(self, x):
+        h = self.encoder(x)
+        z, mu, logvar = self.bottleneck(h)
+        return z, mu, logvar
+
+    def decode(self, z):
+        z = self.fc3(z)
+        z = self.decoder(z)
+        return z
+
+    def forward(self, x):
+        #print(x.shape)
+        z, mu, logvar = self.encode(x)
+        z = self.decode(z)
+        #print(z.shape)
+        return z, mu, logvar
+    
